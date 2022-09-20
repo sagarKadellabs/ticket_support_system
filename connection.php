5954e874e2ddbca06caf779de3f2f1861a7c1f74
Siddharth Mangrora <siddharth.mangrora@bytegage.com>
2022-09-20 17:48:21 +0530

    Code merge

--- connection.php
+++ connection.php
@@ -1,7 +1,3 @@
 <?php
 $con = mysqli_connect('localhost','root','Root4@pass','ticket_support_system') or die('connection failed!');
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> 92c1ea8fd12d6c7d74e03b2282b2ba01bbf815dd
